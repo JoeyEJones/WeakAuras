711f4fbaa97c50e215aa4645ac7d8a07a54aa021
JoeyEJones <JoeyEJones@users.noreply.github.com>
2022-03-20 23:01:35 -0700

    Create LocustRTBBuffs.lua

---
+++ LocustRTBBuffs.lua
@@ -0,0 +1,130 @@
+{
+    ["grow"] = "RIGHT",
+    ["controlledChildren"] = {
+        [1] = "Broadside",
+        [2] = "True Bearing",
+        [3] = "Grand Melee",
+        [4] = "Buried Treasure",
+        [5] = "Ruthless Precision",
+        [6] = "Skull and Crossbones",
+    },
+    ["borderBackdrop"] = "Blizzard Tooltip",
+    ["xOffset"] = -186.88879394531,
+    ["yOffset"] = 434.66696166992,
+    ["anchorPoint"] = "CENTER",
+    ["borderColor"] = {
+        [1] = 0,
+        [2] = 0,
+        [3] = 0,
+        [4] = 1,
+    },
+    ["space"] = 2,
+    ["actions"] = {
+        ["start"] = {
+        },
+        ["init"] = {
+        },
+        ["finish"] = {
+        },
+    },
+    ["triggers"] = {
+        [1] = {
+            ["trigger"] = {
+                ["type"] = "aura2",
+                ["subeventSuffix"] = "_CAST_START",
+                ["event"] = "Health",
+                ["subeventPrefix"] = "SPELL",
+                ["spellIds"] = {
+                },
+                ["names"] = {
+                },
+                ["unit"] = "player",
+                ["debuffType"] = "HELPFUL",
+            },
+            ["untrigger"] = {
+            },
+        },
+    },
+    ["columnSpace"] = 1,
+    ["radius"] = 200,
+    ["selfPoint"] = "LEFT",
+    ["align"] = "CENTER",
+    ["stagger"] = 0,
+    ["subRegions"] = {
+    },
+    ["load"] = {
+        ["size"] = {
+            ["multi"] = {
+            },
+        },
+        ["spec"] = {
+            ["multi"] = {
+            },
+        },
+        ["class"] = {
+            ["multi"] = {
+            },
+        },
+        ["talent"] = {
+            ["multi"] = {
+            },
+        },
+    },
+    ["backdropColor"] = {
+        [1] = 1,
+        [2] = 1,
+        [3] = 1,
+        [4] = 0.5,
+    },
+    ["useLimit"] = false,
+    ["animate"] = false,
+    ["arcLength"] = 360,
+    ["scale"] = 1,
+    ["animation"] = {
+        ["start"] = {
+            ["type"] = "none",
+            ["easeStrength"] = 3,
+            ["duration_type"] = "seconds",
+            ["easeType"] = "none",
+        },
+        ["main"] = {
+            ["type"] = "none",
+            ["easeStrength"] = 3,
+            ["duration_type"] = "seconds",
+            ["easeType"] = "none",
+        },
+        ["finish"] = {
+            ["type"] = "none",
+            ["easeStrength"] = 3,
+            ["duration_type"] = "seconds",
+            ["easeType"] = "none",
+        },
+    },
+    ["border"] = false,
+    ["borderEdge"] = "Square Full White",
+    ["regionType"] = "dynamicgroup",
+    ["borderSize"] = 2,
+    ["sort"] = "none",
+    ["rotation"] = 0,
+    ["fullCircle"] = true,
+    ["constantFactor"] = "RADIUS",
+    ["limit"] = 5,
+    ["borderOffset"] = 4,
+    ["gridType"] = "RD",
+    ["borderInset"] = 1,
+    ["id"] = "LocustRTBBuffs",
+    ["gridWidth"] = 5,
+    ["frameStrata"] = 1,
+    ["anchorFrameType"] = "SCREEN",
+    ["rowSpace"] = 1,
+    ["config"] = {
+    },
+    ["authorOptions"] = {
+    },
+    ["internalVersion"] = 50,
+    ["conditions"] = {
+    },
+    ["information"] = {
+    },
+    ["uid"] = ")tSTdSGIQgh",
+}
